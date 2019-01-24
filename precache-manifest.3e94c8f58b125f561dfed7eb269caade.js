1a39e27cbec02674b2cb61a5e26f8b11469d4768
Charlie <charlie.islas@gmail.com>
2019-01-23 18:43:18 -0600

    Updates

---
+++ precache-manifest.3e94c8f58b125f561dfed7eb269caade.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd845ee937b9e5f2a5a2",
+    "url": "/clicky_game/static/js/runtime~main.bd845ee9.js"
+  },
+  {
+    "revision": "1bfb36716395873cf6f4",
+    "url": "/clicky_game/static/js/main.1bfb3671.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clicky_game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1bfb36716395873cf6f4",
+    "url": "/clicky_game/static/css/main.cf2fe05c.chunk.css"
+  },
+  {
+    "revision": "89ac3b7ad02358854844ff658a76e789",
+    "url": "/clicky_game/index.html"
+  }
+];
