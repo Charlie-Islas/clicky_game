ca71c486f9f00dbbc56db20a868d7d13161f2455
Charlie <charlie.islas@gmail.com>
2019-01-23 19:35:37 -0600

    uploading with images of equal size

--- src/components/CatCard/index.js
+++ src/components/CatCard/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function CatCard(props){
     return(
         <div className="image col-md-3" onClick={()=>props.clickedCat(props.id)}>
-                <img className='img-thumbnail' alt={props.name} src={props.image} />
+                <img className='img-thumbnail' alt={props.name} src={props.image}/>
         </div>
     );
 };
